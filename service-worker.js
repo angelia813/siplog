b96ef12e1c784bc3fdc00516f30d1bdc545c2aad
angelia813 <879741562@qq.com>
2026-08-24 15:38:56 +0800

    修复系统贴纸选中空白与个人饮品库保存

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,9 @@
-const CACHE='siplog-v10';
+const CACHE='siplog-v11';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-180.svg','./icons/icon-512.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin)return;const navigation=event.request.mode==='navigate';event.respondWith(navigation?fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return response}).catch(()=>caches.match('./index.html')):caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}))) });
+
 
 
 
